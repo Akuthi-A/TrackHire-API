3298fbc0d4570aac2a66a472810141942c1582f8
Akuthi <akuthiapril99@gmail.com>
2026-07-04 13:47:26 +0200

    created springboot app

---
+++ trackhire-api/src/main/java/com/trackhire/TrackhireApiApplication.java
@@ -0,0 +1,13 @@
+package com.trackhire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackhireApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackhireApiApplication.class, args);
+	}
+
+}
---
+++ trackhire-api/src/test/java/com/trackhire/TrackhireApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackhire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackhireApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
